dd64ab19d37cb8024e64f753348a3ddc6203d21f
BanDuong <suppersiroo@gmail.com>
2021-12-03 21:30:02 +0700

    add function StudentManagement

--- apps/app_oke1/urls_oke1.py
+++ apps/app_oke1/urls_oke1.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index, name="index"),
+<<<<<<< HEAD
     path('/infor',views.newFunction, name="infor"),
 ]
+=======
+    path('/student',views.StudentManagement, name="add"),
+]
+>>>>>>> 9c63b2d (add function StudentManagement)
--- apps/app_oke1/views.py
+++ apps/app_oke1/views.py
@@ -4,8 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+<<<<<<< HEAD
     context = {'name': 'Ban', 'age': 22, 'gender': 'male'}
     return render(request=request,template_name="app_oke1/index.html",context=context)
 
 def newFuntion(request):
     return render(request, "<p>hello</p>")
+=======
+    #return HttpResponse("oke nhe")
+    return render(request=request,template_name="app_oke1/index.html",context={})
+
+def StudentManagement(request):
+
+    context = {'name': 'Nam', 'age': '22', 'gender': 'male', 'school': 'Haui'}
+    return render(request, template_name="<h1>say hihihi</h1>", context=context)
+>>>>>>> 9c63b2d (add function StudentManagement)
