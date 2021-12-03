4d49b1f046a91a8bb2b9517720e2888a9f4aaa4a
BandG <56988862+BanDuong@users.noreply.github.com>
2021-12-03 22:45:47 +0700

    add note in url

--- apps/app_oke1/urls_oke1.py
+++ apps/app_oke1/urls_oke1.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index, name="index"),
+    # note dc k
     path('/infor',views.newFunction, name="infor"),
+    # note thoi
     path('/school',views.School, name="school"),
 ]
