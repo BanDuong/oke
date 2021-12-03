e88a4d1556c3484c29bebced7cb163a51faaed84
BandG <56988862+BanDuong@users.noreply.github.com>
2021-12-03 22:23:49 +0700

    add School url

--- apps/app_oke1/urls_oke1.py
+++ apps/app_oke1/urls_oke1.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name="index"),
     path('/infor',views.newFunction, name="infor"),
+    path('/school',views.School, name="school"),
 ]
